285eabbffb51db800509c3be5d1df824c3824c22
Aeshvarya Awasthi <aeshvarya310305@gmail.com>
2026-06-25 19:03:50 +0530

    added multi line input

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ load_dotenv()
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-transcript = input("Enter the transcript: ")
+with open("transcript.txt") as f:
+    transcript = f.read()
+
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents=transcript,                         
